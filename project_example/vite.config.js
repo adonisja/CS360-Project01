2c47eda5209ea849d82e78ec8d9ceb7c5b846ca9
IsaiahL <47825885+Isaiah95@users.noreply.github.com>
2025-09-15 13:09:53 +0000

    Fix Vite config: correct nodePolyfills import and usage

--- project_example/vite.config.js
+++ project_example/vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import nodePolyfills from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
     react(),
-    nodePolyfills()
+  nodePolyfills()
   ],
   optimizeDeps: {
     esbuildOptions: {
